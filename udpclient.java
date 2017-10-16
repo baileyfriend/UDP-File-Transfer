b92077ea7840a91045a15ca40665861b2ff94c61
Madison Brooks <brooksma@mail.gvsu.edu>
2017-10-16 00:26:13 -0400

    It sends something....

--- udpclient.java
+++ udpclient.java
@@ -4,6 +4,7 @@ import java.io.*;
 import java.net.*;
 import java.nio.*;
 import java.nio.channels.*;
+import java.util.*;
 
 class udpclient{
 
@@ -13,7 +14,7 @@ class udpclient{
 			@return Int packet number
 		  */
 		public static int getPNum(ByteBuffer bb){
-			bb.position(bb.position() - 1024);
+			bb.position(0); //I was trying to do it with bb.position() - 1024
 
 			byte a = bb.get();
 			byte b = bb.get();
@@ -61,7 +62,7 @@ class udpclient{
 		try{
 			DatagramChannel dc = DatagramChannel.open(); 
 //changing the transport layer protocal
-Console cons = System.console();
+			Console cons = System.console();
 
 			udpclient uc = new udpclient();
       	    String ipStr = "127.0.0.1"; // Default ip address
@@ -104,30 +105,85 @@ Console cons = System.console();
 			}
 			System.out.println("Now using port number " + portInt); //end of checking port number
 
-			// Console cons = System.console();
-			// String ipStr = cons.readLine("Enter an IP Address: ");
-			// String portStr = cons.readLine("Enter a port number: ");
 			int portnum = Integer.parseInt(portStr);
 
-			// @TODO change this so that it doesn't just send the filename as a message, but that it accepts a file back from the server and writes it to the hard drive.
-			Boolean again = true; 
+			Boolean again = true;
+
+			
 			while(again){
 				String m = cons.readLine("Enter a filename to transfer: ");
 				if(m.equals("exit")){
 					again = false;				
 				}
 				ByteBuffer buf = ByteBuffer.wrap(m.getBytes());
-				dc.send(buf,new InetSocketAddress(ipStr,portnum)); 
-			
-				ByteBuffer buffer = ByteBuffer.allocate(4096);
-				SocketAddress clientaddr = dc.receive(buffer); //reading the socket
-				String message = new String(buffer.array());  
-				//returns a socket address of whoever sent the packet
-				System.out.println(message);
-				m = message;
+				dc.send(buf,new InetSocketAddress(ipStr,portnum));  //sending the file name
+				int lastR = 0; //the last packet number recived IN ORDER from the server
+				int packetNum = -1; // a variable to hold the values of the current packet number
+				File newfile = new File(m); //make an empty file with that file name
+				FileOutputStream outstream = new FileOutputStream(newfile); //set up an outputstream with that file
+          		FileChannel fc = outstream.getChannel(); //make the channel for the stream
+				ArrayList<ByteBuffer> buffersthatwehave = new ArrayList<ByteBuffer>();
+				//while loop until the end of the packet
+					//Packet1
+					//for(int x=lastR; x < x+5; x++){ // this is a never ending loop but it is on the right track
+					//	System.out.println("loop is here");
+						ByteBuffer bufff = ByteBuffer.allocate(1024);
+						//int bytesRead = dc.read(bufff);
+						SocketAddress serverAddr = dc.receive(bufff); //receving the packet
+						
+						packetNum = getPNum(bufff); //getting the packet number
+						
+						//if(packetNum == 0){ // need and if statement with a break inorder to stop this loop
+						//	break;
+						//}
+							
+						ByteBuffer akn = ByteBuffer.allocate(4);
+						akn.putInt(packetNum);
+						//dc.send(akn,serverAddr);//sending the aknowlagement- a packet of 4 bytes with just the packet number
+						
+						//if(packetNum == lastR+1){ //making sure this is the next packet we expect to get
+						
+							lastR = packetNum; //incrementing the last recived so we know what in order packets we have
+							//bufff.position(4);
+							//System.out.println(Arrays.toString(bufff.array()));
+							//bufff.flip(); //putting the buffer into reading mode
+							//might need to change position
+							//bufff.position(4);
+							//
+							//byte[] array = bufff.array();
+							//for(byte b:array)
+							//bufff.position(4);
+							//buff.trim();
+							
+							//while(bytesRead != -1){
+								//bufff.flip(); THIS FLIP BREAKS IT
+								//System.out.println(Arrays.toString(bufff.array()));
+								
+		      		    		fc.write(bufff); //writing the buffer into the file and the postion
+		      		    		bufff.compact();//
+		      		    		//bytesRead = dc.read(bufff);
+		      		    	//}
+		      		    	//bufff.compact();//clears the buffer
+						//}
+						//else{} //this packet is not inorder and we should save it into some kind of array?
+
+					//}
+					
+				
 			}
 		}catch(IOException e){
 			System.out.println("Got an IO Exception");
 			}
 		}
-	}
+}	
+	
+	/*
+	@TODO: for the sliding window:
+			-get packet number
+			-keep track of the last packet number we recived IN ORDER
+			-send an aknowlagement for that last packet
+			-keep all packets we recived in an array
+			-ask for missing packets
+			-fill in the spaces where the missing packets were but also utilize the ones we already have
+	*/
+
--- udpserver.java
+++ udpserver.java
@@ -4,23 +4,34 @@ CIS 457-20 */
 import java.io.*;
 import java.net.*;
 import java.nio.*;
+import java.nio.file.*;
 import java.nio.channels.*;
 import java.io.FileOutputStream;
 import java.io.File;
+import java.util.*;
 
 class udpserver{
     
     public udpserver(){} //empty constructor
+    
+/*
+			Returns the packet number of a given buffer/packet
+			@return Int packet number
+*/
+		public static int getPNum(ByteBuffer bb){
+			bb.position(0); //this is 4 only because we know all aknowlagements will be size 4 ;')
 
-    /*Method to make a packtet number into a 4byte array*/
-    public static byte[] getPNumBytes(int number){
-			//will return the number of which packet it is in 4 bytes
-			byte[] pnum = new byte[4];
-			pnum = number.toByteArray;
-			return pnum;
-	}
+			byte a = bb.get();
+			byte b = bb.get();
+			byte c = bb.get();
+			byte d = bb.get();
+
+			int num = ((0xFF & a) << 24) | ((0xFF & b) << 16) | ((0xFF & c) << 8) | (0xFF & d);
+			return num;
+		
+		}
     
-    
+
     public static void main(String args[]){
         
         try{
@@ -42,67 +53,129 @@ class udpserver{
             if(portStr.matches("^[0-9]*$")){
                     portInt = Integer.parseInt(portStr);
                     if(portInt<1024 || 49151<portInt){
-                portNotValid = true;
-                }
-                }else{
+                		portNotValid = true;
+                	}
+            }else{
                     portNotValid = true;
                 }
             while(portNotValid){
                 System.out.println("INVALID PORT NUMBER!");
                 portStr = cons.readLine("Enter port number to listen on: ");
                 if(portStr.matches("^[0-9]*$")){
-                            portInt = Integer.parseInt(portStr);
-                            if(1024<portInt && portInt<49151){
-                            portNotValid = false;
+                	portInt = Integer.parseInt(portStr);
+                    if(1024<portInt && portInt<49151){
+                    	portNotValid = false;
                     }
-                        }else{
-                            portNotValid = true;
-                        }
+                }else{
+                	portNotValid = true;
+                  }
             }
             System.out.println("Now using port number "+portInt);
 
 
             DatagramChannel c = DatagramChannel.open();
             c.bind(new InetSocketAddress(portInt));
-            SocketAddress clientaddr = c.receive(buffer);
-            // @TODO - change this so that it doesn't just send back the message that the client sent, but instead transfers the file
-            String fileStr = new String(buffer.array());
-            fileStr = fileStr.trim(); // have to trim leading and trailing spaces due to making string from oversized buffer
-            File f = new File(fileStr);
+
             
-            String filename = f.getName();
-            boolean fileExists = f.exists();
-            if(fileExists){
-                System.out.println("File exists");
-                
-                long filesize = f.length();
-                //sc.getOutputStream
-                FileInputStream instream = new FileInputStream(f);
-                FileChannel fc = instream.getChannel();
-                ByteBuffer buf = ByteBuffer.allocate(100000);
-                int bytesread = fc.read(buf);
-    
-                
-                
-                while(bytesread != -1){
-                    buf.flip();
-                    c.send(buf, clientaddr);
-                    buf.compact();
-                    bytesread = fc.read(buf);
+            while(true){
+            	ByteBuffer buffer = ByteBuffer.allocate(1024); //our buffer can only be 1024
+            	SocketAddress clientaddr = c.receive(buffer); //reading in the file name b/c its the first thing being sent
+            	System.out.println("A client is requesting information");
+            	
+            	String fileStr = new String(buffer.array());
+            	fileStr = fileStr.trim();
+            	File f = new File(fileStr);
+            	String filename = f.getName();
+
+            	boolean fileExists = f.exists();
+            	
+            	if(fileExists){
+                	System.out.println("File exists");
+                	Path filepath = f.toPath();
+                	int size = (int)Files.size(filepath); // the size in bytes 
+                	int totalP = size/1020; //(hopfully can use this to find out how many packets it is going to take)
+                	
+                	FileInputStream instream = new FileInputStream(f);
+                	FileChannel fc = instream.getChannel();
+                	//ByteBuffer buf = ByteBuffer.allocate(1024);
+                	//int bytesread = fc.read(buf); //might not use this???
+                	int lastA = -1;
+                	int akNum = -1;
+                	int packetNum = 0;
+                	int lastsent = -1;
+                	ArrayList<ByteBuffer> buffersthatwehave = new ArrayList<ByteBuffer>(); //an array list of all the aknowlaged packet numbers
+                	ArrayList<Integer> nums = new ArrayList<Integer>();
+                	//possibly an array list of all of the buffers for easy resend of packets
+                	ByteBuffer bufff = ByteBuffer.allocate(1024);
+                	ByteBuffer akBuff = ByteBuffer.allocate(4);
+                	
+		            	//for(int x=lastA; x < x+5; x++){ // this is a never ending loop but it is on the right track
+		            	//	System.out.println("loop is here");
+		            		bufff.putInt(packetNum); //putting the packet number into the first 4 bytes of the buffer
+		            		fc.read(bufff); //filling the rest of the buffer with information from the file
+		            		bufff.flip();
+		            		System.out.println(Arrays.toString(bufff.array()));
+		            		c.send(bufff,clientaddr);
+		            		lastsent = packetNum;
+		            		packetNum = packetNum+1;
+							bufff.compact();
+							//c.receive(akBuff); //receiving an aknowlagement
+							//akNum = getPNum(akBuff);
+							//nums.add(akNum);
+							//if (akNum == 0){ //this was the last packet we had to send so we want to break the loop.
+							//	break;
+							//}
+							//if(lastA != lastsent -4){ //if we need to resent a packet
+							//	break;
+		            		//	}
+                		//}
                 }
-            }
-            // while(true){
-            //     ByteBuffer buffer = ByteBuffer.allocate(4096);
-            //     //SocketAddress clientaddr = c.receive(buffer); //reading the socket
-            //     String message = new String(buffer.array());  
-            //     //returns a socket address of whoever sent the packet
-            //     System.out.println(message);
-            //     buffer.flip();
-            //     c.send(buffer,clientaddr); //will this work
-            // }
+             }
         }catch(IOException e){
             System.out.println("Got an IO Exception");
         }
 
     }
 }
+	
+	/*Code below is for sending a file
+							buf.flip();
+				  		    c.send(buf, clientaddr);
+				  		    buf.compact();
+							bytesread = fc.read(buf);
+							currWindow++;
+
+
+
+	class slidingWindow(){
+	
+		public static byte[] getPNumBytes(int number){
+			//will return the number of which packet it is in 4 bytes
+			byte[] pnum = new byte[4];
+			pnum = number.toByteArray;
+			return pnum;
+		}
+		public static boolean isAknowlaged(){
+			//will return whether or not the package was aknowlaged by the client.
+		}
+	
+	
+	
+	@TODO: for the sliding window:
+			-send packet number with packet
+			-for simplicity just number packets 1 to 4billion (no need to wrap around and reuse numbers)
+			-only send 5 packets at a time
+			-recive aknowlagements for all packets and keep the last one that is IN ORDER 
+				for example if we get aknowlagements 1 2 4 5 then the variable last aknowlaged would be 2
+			-HAVE variable last packet recived (as decribed above)
+			-keep all packets we sent in an array (the contents of the buffers)
+			-resend missing packets
+				also off of the example above (aknowlagements for 1 2 4 5) MOVE the window by sending packets 6 and 7
+	*/
+	
+	
+	
+	
+	
+	
+	
